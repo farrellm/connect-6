4ec3532fa981139c858580189850d95ca4a3caae
Matthew FArrell <farrellm23@gmail.com>
2012-12-01 11:33:26 -0500

    start game loop

--- src/connect_6/core.clj
+++ src/connect_6/core.clj
@@ -1,10 +1,27 @@
 (ns connect-6.core
+  (:require [ connect-6.game :as game])
+  (:require [ connect-6.player.human :as human])
+  (:use [slingshot.slingshot :only [throw+ try+]])
   (:gen-class :main true))
 
-(defn -main []
-    (println "Hello World!"))
+(defrecord Round [board player1 player2])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn play [board player plays])
+
+(defn first-round [state]
+  (play (:board state) (:player2 state) 1))
+
+(defn game-loop [state]
+  #(game-loop)
+  (play board player1 2)
+  (play board player2 2)
+  )
+
+(defn -main []
+  (println "Connect-6")
+
+  (let [s0 {:board (game/make-board 16)
+            :player1 (human/make-player)
+            :player1 (human/make-player)}
+        s1 (first-round s0)]
+    (trampoline game-loop)))
